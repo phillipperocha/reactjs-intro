2a40fc75d14a0c867a23e2830bab2122272f9112
Phillippe Rocha <phillipperocha@gmail.com>
2019-12-08 16:25:58 -0300

    Importando imagens

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.css'
 
+import homemamanha from './assets/dev.png'
+
 function App() {
-  return <h1>Hello Rottein!</h1>
+  return <img width="200" src={homemamanha} />
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,12 @@ module.exports = {
           { loader: 'style-loader' },
           { loader: 'css-loader' }
         ]
+      },
+      {
+        test: /.*\.(gif|png|jpe?g)$/i,
+        use: {
+          loader: 'file-loader'
+        }
       }
     ]
   }
